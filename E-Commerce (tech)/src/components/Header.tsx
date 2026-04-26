448e47ee8421b2e6d705ebc2892e3f71a805083b
HaihengDev <haihenglim9@gmail.com>
2026-04-26 22:35:44 +0700

    feat: enhance header component with navigation and icons, add Font Awesome for icons

--- E-Commerce (tech)/src/components/Header.tsx
+++ E-Commerce (tech)/src/components/Header.tsx
@@ -1,7 +1,17 @@
+import NavBar from './NavBar';
+
 export default function Header() {
   return (
-    <header>
-      <h2>TECHNE</h2>
+    <header className="flex justify-between items-center p-2.5">
+      <aside className="flex gap-8 items-center">
+        <h2 className="font-bold text-3xl uppercase">Techne</h2>
+        <NavBar />
+      </aside>
+
+      <div className="icons">
+        <i className="fa-solid fa-cart-shopping"></i>
+        <i className="fa-solid fa-circle-user"></i>
+      </div>
     </header>
   );
 }
---
+++ E-Commerce (tech)/src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Categories</a>
+      <a href="#">Service</a>
+      <a href="#">Contact</a>
+    </nav>
+  );
+}
